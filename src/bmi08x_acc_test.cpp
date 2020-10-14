c77b639ffc5c07c68d6bec59e1ebee5905f4f297
patricia <waa300386@gmail.com>
2020-10-14 14:43:03 +0200

    make things of the tester cleaner

--- src/bmi08x_acc_test.cpp
+++ src/bmi08x_acc_test.cpp
@@ -22,37 +22,23 @@ void set_default(bmi08x_dev *dev){
     dev->gyro_cfg.bw = BMI08X_GYRO_BW_23_ODR_200_HZ;
 }
 
-int main(){
+int main(){ //main
     int8_t rslt = BMI08X_OK;
     std::cout << "First: " << BMI08X_OK << std::endl;
     uint8_t data = 0;
     bmi08x_dev dev;
     dev = bmi.initialize(rslt);
    
-    // get gyro power mode
+    // get power mode
     rslt = bmi08g_get_power_mode(&dev);
     rslt = bmi08a_get_power_mode(&dev);
-//    set_default(&dev);
 
-    
-    //    rslt = bmi08a_set_regs(BMI08X_REG_ACCEL_PWR_CTRL, &cmd, 1, &sensor);
-//    rslt = bmi08a_set_power_mode(&dev);    
-//    set_default(&dev);
-//    rslt = bmi08a_set_meas_conf(&dev);
+    rslt = bmi08g_set_power_mode(&dev);
     rslt = bmi08a_get_meas_conf(&dev);   
-//    rslt = bmi08a_get_regs(0x7D, &data, 1, &dev);
-    
-
-
-//    while(rslt == BMI08X_OK && dev.accel_cfg.power == BMI08X_ACCEL_PM_ACTIVE){
-//        rslt = bmi08a_get_data(&raw_acc, &dev);
-//	std::cout << "raw_acc_x:" << raw_acc.x << " raw_acc_y:" << raw_acc.y << " raw_acc_z:" << raw_acc.z << std::endl;
-//    }
 
 
 //    // talk to gyro
 //    struct bmi08x_sensor_data raw_gyro;
-//    rslt = bmi08g_set_power_mode(&dev);
 //    set_default(&dev);
 //    while (rslt == BMI08X_OK){
 //    rslt = bmi08g_get_data(&raw_gyro, &dev);
@@ -67,7 +53,7 @@ int main(){
     set_default(&dev);
     rslt = bmi08a_set_power_mode(&dev);
     rslt = bmi08a_set_meas_conf(&dev);
-    while (rslt == BMI08X_OK){
+    while (rslt == BMI08X_OK && dev.accel_cfg.power == BMI08X_ACCEL_PM_ACTIVE){
     rslt = bmi08a_get_data(&raw_acc, &dev);
     std::cout << std::fixed << "raw_acc_x: " << raw_acc.x << "\n raw_acc_y:" << raw_acc.y << "\n raw_acc_z:" << raw_acc.z << std::endl;
     rslt = bmi08a_get_sensor_temperature(&dev, &raw_tmp);
